35c9b54395d1cb15a5b5208b88e76f3e3c4977d9
Rohan29-code <pattapagalurohansai@gmail.com>
2026-03-19 01:51:12 +0530

    UC13: Performance Comparison

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,91 @@
-public class main {
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Main {
+
+    public static boolean stackMethod(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+            stack.push(ch);
+        }
+
+        for (char ch : str.toCharArray()) {
+            if (ch != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean dequeMethod(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        Deque<Character> deque = new LinkedList<>();
+
+        for (char ch : str.toCharArray()) {
+            deque.addLast(ch);
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean twoPointerMethod(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("=====================================");
-        System.out.println("   Welcome to Palindrome Checker App ");
-        System.out.println("=====================================");
+        System.out.println("Enter a string:");
+        String input = sc.nextLine();
 
-        System.out.println("Version: 1.0");
+        long startTime, endTime;
 
-        System.out.println("This application checks whether a given string is a palindrome.");
+        startTime = System.nanoTime();
+        boolean result1 = stackMethod(input);
+        endTime = System.nanoTime();
+        long stackTime = endTime - startTime;
 
-        System.out.println("\nProgram started successfully...");
+        startTime = System.nanoTime();
+        boolean result2 = dequeMethod(input);
+        endTime = System.nanoTime();
+        long dequeTime = endTime - startTime;
+
+        startTime = System.nanoTime();
+        boolean result3 = twoPointerMethod(input);
+        endTime = System.nanoTime();
+        long twoPointerTime = endTime - startTime;
+
+        System.out.println("\nResults:");
+        System.out.println("Stack Method: " + result1 + " | Time: " + stackTime + " ns");
+        System.out.println("Deque Method: " + result2 + " | Time: " + dequeTime + " ns");
+        System.out.println("Two Pointer Method: " + result3 + " | Time: " + twoPointerTime + " ns");
+
+        sc.close();
     }
 }
